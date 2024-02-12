808b83f63e0ca56492418a4a375003567b568b5c
Galina Dei <gal-dej@yandex.ru>
2024-02-12 18:02:34 +0300

    Task two has done

---
+++ Task_two.py
@@ -0,0 +1,12 @@
+# 3. Напишите программу, которая принимает две строки вида “a/b” - дробь с числителем и знаменателем.
+# Программа должна возвращать сумму и произведение* дробей. Для проверки своего кода используйте модуль fractions
+
+import fractions
+import math
+
+a1, b1 = [int(i) for i in input('Введите дробь вида а/в: ').split('/')]
+a2, b2 = [int(i) for i in input('Введите вторую дробь вида а/в: ').split('/')]
+
+gdc = math.gcd((a1*b2+a2*b1), b1*b2)
+print(int((a1*b2+a2*b1)/gdc), int(b1*b2/gdc), sep='/')
+print(fractions.Fraction(a1, b1) + fractions.Fraction(a2, b2))
